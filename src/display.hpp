edb377a7a84f339ab2ffac084ba160af458b4ae8
Laurynas <laurynas.lubys@gmail.com>
2020-10-17 17:53:07 +0300

    Very blinky

--- src/display.hpp
+++ src/display.hpp
@@ -9,7 +9,10 @@ enum Channel
 };
 
 void display_num(Channel channel, int low, int high);
+void display_bits(Channel channel, bool low_1, bool low_2, bool low_3, bool high_1, bool high_2, bool high_3);
 void setup_display();
 void activate_channel(Channel channel);
+void start_blink(Channel channel, int on_millis, int off_millis);
+void stop_blink(Channel channel);
 
 #endif
--- src/division.cpp
+++ src/division.cpp
@@ -4,12 +4,18 @@
 #include <Arduino_FreeRTOS.h>
 
 const int division_values[] = {
-    24, //QUARTER,
-    16, //QUARTER_TRIP,
-    12, //EIGHT,
-    8,  //EIGHT_TRIP,
-    6,  //SIXTEENTH,
-    4,  //SIXTEENTH_TRIP,
+    192, //TWO_BAR
+    128, //TWO_BAR_TRIP
+    96,  //BAR
+    64,  //BAR_TRIP
+    48,  //HALF
+    32,  //HALF_TRIP
+    24,  //QUARTER,
+    16,  //QUARTER_TRIP,
+    12,  //EIGHT,
+    8,   //EIGHT_TRIP,
+    6,   //SIXTEENTH,
+    4,   //SIXTEENTH_TRIP,
 };
 
 int current_division = -1;
@@ -20,7 +26,7 @@ void SamplePotentiometer(void *pvParam)
   for (;;)
   {
     int potValue = analogRead(A0);
-    int current_division_idx = map(potValue, 0, 1023, 0, 5);
+    int current_division_idx = map(potValue, 0, 1023, 0, 11);
     int new_division = division_values[current_division_idx];
     if (current_division == new_division)
     {
@@ -35,7 +41,26 @@ void SamplePotentiometer(void *pvParam)
       same_value_count = 0;
       activate_channel(CLOCK_DIV);
     }
-    display_num(CLOCK_DIV, current_division_idx + 1, current_division_idx - 2);
+    if (current_division_idx >= 6)
+    {
+      display_bits(CLOCK_DIV,
+                   current_division_idx >= 6,
+                   current_division_idx >= 7,
+                   current_division_idx >= 8,
+                   current_division_idx >= 9,
+                   current_division_idx >= 10,
+                   current_division_idx >= 11);
+    }
+    else
+    {
+      display_bits(CLOCK_DIV,
+                   current_division_idx <= 0,
+                   current_division_idx <= 1,
+                   current_division_idx <= 2,
+                   current_division_idx <= 3,
+                   current_division_idx <= 4,
+                   current_division_idx <= 5);
+    }
     vTaskDelay(50 / portTICK_PERIOD_MS);
     current_division = new_division;
   }
@@ -49,4 +74,6 @@ void setup_divisions()
               NULL,
               3,
               NULL);
+
+  start_blink(CLOCK_DIV, 50, 30);
 }
