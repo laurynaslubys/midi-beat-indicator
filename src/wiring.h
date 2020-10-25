9080c1ee61cd56bbbd86a4f51fed6ee2d6c3c432
Laurynas <laurynas.lubys@gmail.com>
2020-10-25 15:34:52 +0200

    Schematics + wiring for PCB version

--- src/wiring.h
+++ src/wiring.h
@@ -15,12 +15,12 @@
 
 #elif WIRING_PRO_MICRO
 
-#define LOW_1 4
-#define LOW_2 7
+#define LOW_1 7
+#define LOW_2 8
 #define LOW_3 9
-#define HIGH_1 5
-#define HIGH_2 6
-#define HIGH_3 8
+#define HIGH_1 6
+#define HIGH_2 5
+#define HIGH_3 4
 #define SYNC_BUTTON_PIN 3
 #define TRIG_PIN 10
 #define POT_PIN A0
